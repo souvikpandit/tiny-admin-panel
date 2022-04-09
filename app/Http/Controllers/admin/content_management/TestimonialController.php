1e1c907706ff040baf897bdf9cac5eb6eb9522ee
souvikpandit <48911816+souvikpandit@users.noreply.github.com>
2022-04-09 22:15:52 +0530

    Update TestimonialController.php

--- app/Http/Controllers/admin/content_management/TestimonialController.php
+++ app/Http/Controllers/admin/content_management/TestimonialController.php
@@ -32,58 +32,14 @@ class TestimonialController extends Controller
         return view('admin.content_management.testimonial.add',compact('sorting_no'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         $testimonial = Post::where([['id',$id],['post_type','testimonial']])->first();
         return view('admin.content_management.testimonial.edit', compact('testimonial'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy($id)
     {
         Post::where('id',$id)->delete();
